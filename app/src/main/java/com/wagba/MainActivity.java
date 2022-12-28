9e9670d976e47cd2742d09c81898ec1e44343390
Ahmed Magdy <ahmedmagdy373@gmail.com>
2022-12-28 20:01:21 +0200

    Profile

--- app/src/main/java/com/wagba/MainActivity.java
+++ app/src/main/java/com/wagba/MainActivity.java
@@ -8,8 +8,11 @@ import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.wagba.data.firebase.FirebaseHelper;
 import com.wagba.databinding.ActivityMainBinding;
+
+import org.checkerframework.common.subtyping.qual.Bottom;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,10 +24,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
         binding = ActivityMainBinding.inflate(getLayoutInflater());
-
-
         setContentView(binding.getRoot());
-
+        BottomNavigationView navigationView = findViewById(R.id.bottomAppBar);
+        navigationView.setBackground(null);
+        navigationView.getMenu().getItem(2).setEnabled(false);
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
         appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
         if (FirebaseHelper.getCurrentUser() != null) {
